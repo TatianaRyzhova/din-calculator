047c9e0f91c3e7543455dbceef2c132ce59fa0c1
Tatiana.ryzhova.51 <tatiana.ryzhova.51@yandex.ru>
2022-03-09 17:49:47 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,89 @@
-import logo from './logo.svg';
+import React, {useEffect, useState} from "react";
 import './App.css';
 
 function App() {
+const [number, setNumber] = useState({
+  a: '',
+  b: '',
+  c: ''
+});
+const [result, setResult] = useState('');
+const [submitted, setSubmitted] = useState(false);
+
+function handleChange(event) {
+  const input = event.target;
+  const value = input.value;
+  const name = input.name;
+  setNumber({
+    ...number,
+    [name]: value
+  });
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  setSubmitted(true);
+
+  // alert(`${number.a} + ${number.b} + ${number.c}`);
+  // alert(parseInt(number.a) + parseInt(number.b) + parseInt(number.c))
+  const x = 0.3 * Number(number.a)
+  const y = 0.75 * Number(number.b)
+  const z = 0.46 * Number(number.c)
+  setResult( x + y + z);
+
+  setNumber({
+    a: '',
+    b: '',
+    c: ''
+  })
+  setSubmitted(false);
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <div className="container">
+      <h1>My Calculator</h1>
+      <p>The following formula is used for the calculations:</p>
+      <p>А = 0,3 * а + 0,75 * b + 0,46 * с</p>
+
+      <form onSubmit={handleSubmit}>
+        <label> a: </label>
+        <input 
+        type="number" 
+        name="a" 
+        onChange={handleChange}
+        value={number.a}
+        submitted={submitted}
+        required
+        />
+
+        <label> b: </label>
+        <input 
+        type="number" 
+        name="b"
+        onChange={handleChange}
+        value={number.b}
+        submitted={submitted}
+        required
+        />
+
+        <label> c: </label>
+        <input 
+        type="number" 
+        name="c"
+        onChange={handleChange}
+        value={number.c}
+        submitted={submitted}
+        required
+        />
+        
+        <button type="submit">CALCULATE</button>
+      </form>
+
+      <div className="result">
+        <p>Result: 
+          <span> {result}</span>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
     </div>
   );
 }
