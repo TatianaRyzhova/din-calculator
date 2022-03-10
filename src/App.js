7b146a83e2069258770aba48c19c490db3fe97eb
Tatiana.ryzhova.51 <tryzhova93@gmail.com>
2022-03-10 20:33:08 +0100

    add missing parameters

--- src/App.js
+++ src/App.js
@@ -5,7 +5,18 @@ function App() {
 const [number, setNumber] = useState({
   a: '',
   b: '',
-  c: ''
+  c: '',
+  d: '', 
+  e: '',
+  f: '',
+  g: '',
+  h: '',
+  i: '',
+  j: '',
+  k: '',
+  l: '',
+  m: '',
+  n: ''
 });
 const [result, setResult] = useState('');
 const [submitted, setSubmitted] = useState(false);
@@ -34,7 +45,18 @@ function handleSubmit(event) {
   setNumber({
     a: '',
     b: '',
-    c: ''
+    c: '',
+    d: '', 
+    e: '',
+    f: '',
+    g: '',
+    h: '',
+    i: '',
+    j: '',
+    k: '',
+    l: '',
+    m: '',
+    n: ''
   })
   setSubmitted(false);
 }
@@ -42,46 +64,153 @@ function handleSubmit(event) {
   return (
     <div className="container">
       <h1>My Calculator</h1>
-      <p>The following formula is used for the calculations:</p>
-      <p>А = 0,3 * а + 0,75 * b + 0,46 * с</p>
+      {/* <p>The following formula is used for the calculations:</p>
+      <p>А = 0,3 * а + 0,75 * b + 0,46 * с</p> */}
 
       <form onSubmit={handleSubmit}>
-        <label> a: </label>
-        <input 
-        type="number" 
-        name="a" 
-        onChange={handleChange}
-        value={number.a}
-        submitted={submitted}
-        required
-        />
+        <div className="parameters">
+          <div className="parameters-first">
+            <label> a: </label>
+            <input
+              type="number"
+              name="a"
+              onChange={handleChange}
+              value={number.a}
+              submitted={submitted}
+              required
+            />
+            <label> b: </label>
+            <input
+              type="number"
+              name="b"
+              onChange={handleChange}
+              value={number.b}
+              submitted={submitted}
+              required
+            />
+            <label> c: </label>
+            <input
+              type="number"
+              name="c"
+              onChange={handleChange}
+              value={number.c}
+              submitted={submitted}
+              required
+            />
 
-        <label> b: </label>
-        <input 
-        type="number" 
-        name="b"
-        onChange={handleChange}
-        value={number.b}
-        submitted={submitted}
-        required
-        />
+            <label> d: </label>
+            <input
+              type="number"
+              name="d"
+              onChange={handleChange}
+              value={number.d}
+              submitted={submitted}
+              required
+            />
+            <label> e: </label>
+            <input
+              type="number"
+              name="e"
+              onChange={handleChange}
+              value={number.e}
+              submitted={submitted}
+              required
+            />
+            <label> f: </label>
+            <input
+              type="number"
+              name="f"
+              onChange={handleChange}
+              value={number.f}
+              submitted={submitted}
+              required
+            />
+            <label> g: </label>
+            <input
+              type="number"
+              name="g"
+              onChange={handleChange}
+              value={number.g}
+              submitted={submitted}
+              required
+            />
+          </div>
 
-        <label> c: </label>
-        <input 
-        type="number" 
-        name="c"
-        onChange={handleChange}
-        value={number.c}
-        submitted={submitted}
-        required
-        />
-        
+          <div className="parameters-second">
+            <label> h: </label>
+            <input
+              type="number"
+              name="h"
+              onChange={handleChange}
+              value={number.h}
+              submitted={submitted}
+              required
+            />
+            <label> i: </label>
+            <input
+              type="number"
+              name="i"
+              onChange={handleChange}
+              value={number.i}
+              submitted={submitted}
+              required
+            />
+            <label> j: </label>
+            <input
+              type="number"
+              name="j"
+              onChange={handleChange}
+              value={number.j}
+              submitted={submitted}
+              required
+            />
+
+            <label> k: </label>
+            <input
+              type="number"
+              name="k"
+              onChange={handleChange}
+              value={number.k}
+              submitted={submitted}
+              required
+            />
+            <label> l: </label>
+            <input
+              type="number"
+              name="l"
+              onChange={handleChange}
+              value={number.l}
+              submitted={submitted}
+              required
+            />
+            <label> m: </label>
+            <input
+              type="number"
+              name="m"
+              onChange={handleChange}
+              value={number.m}
+              submitted={submitted}
+              required
+            />
+            <label> n: </label>
+            <input
+              type="number"
+              name="n"
+              onChange={handleChange}
+              value={number.n}
+              submitted={submitted}
+              required
+            />
+          </div>
+        </div>
+
         <button type="submit">CALCULATE</button>
       </form>
 
       <div className="result">
-        <p>Result: 
-          <span> {result}</span>
+        <p>
+          Result:
+          <span className={result > 0.259979 ? 'result-red' : 'result-green'}> {result}</span>
         </p>
       </div>
     </div>
